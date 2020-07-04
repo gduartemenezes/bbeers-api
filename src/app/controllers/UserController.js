2c58478e57a8f73997c67ef1aa340112c9563b6e
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-04 10:37:45 -0300

    Update UserCtrl.store, add yup data validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(6),
+    });
     try {
+      if (!(await schema.isValid(req.body))) {
+        return res.json({ error: 'Invalid data, try again' });
+      }
+
       const { email } = req.body;
       const userExists = User.findOne({ where: { email } });
 
