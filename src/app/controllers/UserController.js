d30f75f4cda0eebea82e4cb4c86967af6c3e919a
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-04 09:24:09 -0300

    Update UserCtrl.store, add email verification

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,6 +3,12 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
+      const { email } = req.body;
+      const userExists = User.findOne({ where: { email } });
+
+      if (userExists)
+        return res.status(401).json({ message: 'E-mail already in use' });
+
       const user = await User.create(req.body);
 
       return res.json(user);
