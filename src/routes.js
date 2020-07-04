0e57f5f5daa18d3c93282d1144b50df38a896886
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-04 09:15:38 -0300

    Add first request to routes, UserController.store

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
