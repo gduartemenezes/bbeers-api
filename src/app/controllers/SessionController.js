0df17643ff678f5ed12079c00ffc5b073f5d4d46
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-06 14:58:09 -0300

    Add Yup data validation do SessionsCtrl.store

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,16 +11,17 @@ class SessionController {
       password: Yup.string().required().min(6),
     });
 
-    if (!(await schema.isValid(req.body)))
+    if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Invalid data, try again' });
+    }
     const { email, password } = req.body;
-    const user = User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
 
     if (!user) return res.status(400).json({ error: 'E-mail does not match' });
 
-    if (!(await user.checkPassword(password)))
+    if (!(await user.checkPassword(password))) {
       return res.status(400).json({ error: 'Password does not match' });
-
+    }
     const { id, name } = user;
 
     return res.json({
@@ -32,4 +33,4 @@ class SessionController {
   }
 }
 
-export default SessionController;
+export default new SessionController();
