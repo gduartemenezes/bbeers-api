1e87c3b07c2dc8eb887f1b50a45034a65f081271
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-06 15:07:00 -0300

    Add authMiddleware to control users traffic

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware());
 
 export default routes;
