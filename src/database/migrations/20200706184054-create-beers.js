d48c4fbdb5839f1b9ef2595e1980a28d6063b673
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-07 18:22:39 -0300

    Fix: undo and update migrations

--- src/database/migrations/20200706184054-create-beers.js
+++ src/database/migrations/20200706184054-create-beers.js
@@ -48,7 +48,16 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       },
-
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
